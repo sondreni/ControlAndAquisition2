9496686b72337959b97f72c60f872f06b6541ed8
Eirik Siljan <eirik_siljan@hotmail.com>
2016-04-19 14:17:48 +0200

    Created PI controller

--- ControlAndAquisition/ControlAndAquisition/PIController.cs
+++ ControlAndAquisition/ControlAndAquisition/PIController.cs
@@ -2,11 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ControlAndAquisition
 {
     class PIController
     {
+        double P;
+        double Kp;
+        double e;
+        double Ts;
+        double Ti;
+        double I;
+        double U;
+        double MaxU;
+        double MinU;
+
+        double Compute()
+        {
+            P = Kp * e;
+            I = I + e * (Kp * Ts) / Ti;
+
+
+            if (I > MaxU)
+            {
+                I = MaxU;
+
+            }
+            else if(I<MinU)
+            {
+                    I = MinU;
+                
+            }
+
+            if (P + I > MaxU)
+            {
+                U = MaxU;
+            }
+
+            else if (P + I < MinU)
+            {
+
+            }
+            else
+            {
+                U = P + I;
+            }
+
+            return U;
+        }
     }
 }
