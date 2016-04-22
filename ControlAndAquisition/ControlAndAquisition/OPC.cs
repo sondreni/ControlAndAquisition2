9dbca0a5a2d09fa0b268f635731a37aa8b5ad566
Eirik Siljan <eirik_siljan@hotmail.com>
2016-04-22 12:06:32 +0200

    Sondre må slette OOADP oppg

--- ControlAndAquisition/ControlAndAquisition/OPC.cs
+++ ControlAndAquisition/ControlAndAquisition/OPC.cs
@@ -20,7 +20,7 @@ namespace ControlAndAquisition
         DataSocket dataSocketU = new DataSocket();
         DataSocket dataSocketR = new DataSocket();
         
-        
+        //HJELP SONDRE
 
         public void opcConnectSockets()
         {
