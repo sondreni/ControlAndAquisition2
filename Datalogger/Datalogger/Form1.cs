88aa50f6943404b3934b0f59b07561fbdd8511a4
Sondre Nilsen <sondreni@gmail.com>
2016-04-25 15:40:35 +0200

    Started The DataLogger, Not working yet

--- Datalogger/Datalogger/Form1.cs
+++ Datalogger/Datalogger/Form1.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Datalogger
 {
     public partial class Form1 : Form
     {
+        private string connectionString = ConfigurationManager.ConnectionStrings["LibraryDBConnectionString"].ConnectionString;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void test()
+        {
+            SqlConnection sqlConnection1 = new SqlConnection();
+
+            sqlConnection1.ConnectionString="Data Source=ServerName;" +"Initial Catalog=DataBaseName;" +"User id=UserName;" +"Password=Secret;";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT Region (RegionID, RegionDescription) VALUES (5, 'NorthWestern')";
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection1.Close();
+        }
+
+
+
+        private void SendIntoSQL()
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO Data (Name, PhoneNo, Address) VALUES (@Name, @PhoneNo, @Address)");
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@PhoneNo", txtPhone.Text);
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+
+
+
+
     }
 }
